6add62abdf657e0548f1b84463586a74c7dad174
NoopKing <120143429+NoopKing@users.noreply.github.com>
2023-04-23 15:28:20 -0300

    commit-checker

---
+++ api/core/controles/index.js
@@ -0,0 +1,53 @@
+import request from "../models/index.js"
+import ultilsCC from "../Ultils/index.js"
+import Cielo1 from "../checkers/cielo-1.js"
+import Rede1 from "../checkers/rede1.js"
+import {
+  searchData,
+  ccIsValidAndResult
+} from "../Ultils/functions.js"
+
+
+
+const functionalities = {
+
+  "rede1": async function (checker, cc) {
+    
+    const dataBinAndResult = await ccIsValidAndResult({
+      cc: cc,
+      ultilsCC: ultilsCC,
+      request: request
+    })
+
+    cc = ultilsCC["destructCC"](cc)
+    if (!cc) return {
+      success: false,
+      result: "algo de errado com a cc"
+    }
+    if (!dataBinAndResult.success) return dataBinAndResult
+   const {body} = await request({
+     method: "POST",
+     url:"https://www.4devs.com.br/ferramentas_online.php",
+     form:"acao=gerar_pessoa&sexo=I&pontuacao=S&idade=0&cep_estado=&txt_qtde=1&cep_cidade="
+   }) 
+  
+    return Rede1({
+      "searchData": searchData,
+      "request": request,
+      "dataBin": dataBinAndResult.result,
+      "cc": cc,
+      "checker": checker["data-Requests"],
+      "dataPeople": body,
+    })
+  }
+}
+
+async function execCheckes({nameChecker, checkerRequisitado, cc}) {
+  return await functionalities[nameChecker](checkerRequisitado[nameChecker], cc)
+
+}
+
+
+export {
+  execCheckes
+}
--- api/core/index.js
+++ api/core/index.js
@@ -1,12 +1,53 @@
 import api from 'lambda-api'
+import {execCheckes} from "./controles/index.js"
 const server = api()
+
+const dataCHecker = {
+    "rede1": {
+      "data-Requests": {
+        "headers": {
+          "scheme": "https",
+          "accept": "application/json, text/javascript, */*; q=0.01",
+  
+          "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+  
+          "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+  
+  
+          "user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13F69 MicroMessenger/6.6.1 NetType/4G Language/zh_CN",
+  
+          "x-requested-with": "XMLHttpRequest"
+        },
+        "form": "product_id=7768&product_sku=4491&quantity=1",
+      },
+      "data-client": {
+        "name":"rede1",
+        "status": "online",
+        "online": true,
+        "value":"16,00",
+        "about": "checker rede legalzinho",
+        "type":"rede"
+      }
+    }
+   }
 
 // Define a route that handles POST requests
 server.post('/', async (req, res) => {
   // Access dynamic values from the request body
-  const value1 = req.body?.cc
-
-  return { value: value1 };
+   let result
+       try {
+          result = await execCheckes({
+      nameChecker:"rede1",
+      checkerRequisitado:dataCHecker,
+      cc: "5162921881334213|11|2030|521"
+    })
+       } catch (e) {
+         result = {
+           success:false,
+           result:"erro ao fazer as requests"
+         }
+       }
+  return { value: resultcomm };
 });
 
 // Declare your Lambda handler
