5633e4763007d5467f246f536d1aa3a31f679aa2
NoopKing <120143429+NoopKing@users.noreply.github.com>
2023-04-23 14:53:53 -0300

    sla

--- api/core/index.js
+++ api/core/index.js
@@ -1,1 +1,12 @@
-module.exports = require('./dist/index.js');
+import { APIGatewayEvent, Context } from 'aws-lambda';
+import createAPI from 'lambda-api';
+
+const api = createAPI();
+
+api.get('/', async (req, res) => {
+    return { status: 'Hello from Function!!' };
+});
+
+export async function handler(event,  Context) {
+    return await api.run(event, context);
+};
