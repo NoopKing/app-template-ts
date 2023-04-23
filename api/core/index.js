330af73c111f500d63fdf41c6b7e49e6f3eb777e
NoopKing <120143429+NoopKing@users.noreply.github.com>
2023-04-23 15:19:41 -0300

    comit

--- api/core/index.js
+++ api/core/index.js
@@ -1,13 +1,17 @@
 import api from 'lambda-api'
 const server = api()
-// Define a route
-server.get('/', async (req, res) => {
-  return { status: 'ok' };
+
+// Define a route that handles POST requests
+server.post('/', async (req, res) => {
+  // Access dynamic values from the request body
+  const value1 = req.body?.cc
+
+  return { value: value1 };
 });
 
 // Declare your Lambda handler
 export async function handler (event, context)  {
   // Run the request
   return await server.run(event, context);
-};;
+};
 
