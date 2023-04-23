d6fa4260e8cd7b191515a595b8d38b4a05a97721
NoopKing <120143429+NoopKing@users.noreply.github.com>
2023-04-23 15:08:40 -0300

    al

--- api/core/index.js
+++ api/core/index.js
@@ -1,13 +1,13 @@
 import api from 'lambda-api'
-api()
+const server = api()
 // Define a route
-api.get('/', async (req, res) => {
+server.get('/', async (req, res) => {
   return { status: 'ok' };
 });
 
 // Declare your Lambda handler
 exports.handler = async (event, context) => {
   // Run the request
-  return await api.run(event, context);
+  return await server.run(event, context);
 };;
 
